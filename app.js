f1d81d358ca9adc80aaca1a885627f02c7778138
Erica Bruenton <bosslady@Ericas-MacBook-Air.local>
2018-06-15 17:16:22 -0600

    Get Even answered

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("what's broken now?");
+
+//Easy Going
+ // for ( i = 1; i < 21; i++) {
+	// console.log(i);
+ // }
+
+//Get Even
+
+for (i = 0; i < 201; i += 2){
+ 	console.log(i);
+ }
--- js/app.js
+++
@@ -1,7 +0,0 @@
-//Easy Going
-for ( i = 1; i < 21; i++) {
-	console.log(i);
-}
-
-//Get Even
-
